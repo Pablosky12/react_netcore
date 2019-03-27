6309a0d5994ba467c38a72762b32e671ab8ee464
pablosky12 <pablo1012@outlook.com>
2019-03-26 22:33:58 -0300

    Initial progress

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,12 @@
+using angular_netcore.Persistance;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -22,7 +25,8 @@ namespace angular_netcore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+            services.AddAutoMapper();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
